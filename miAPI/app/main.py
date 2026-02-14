7203157efeaf2b93dc1861d335dcf88f10405547
Gabriel Valencia <gabrielvalenciaolvera@gmail.com>
2026-02-13 20:32:04 -0600

    funcionalidades de CRUD con APIs

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,5 +1,5 @@
 #Importaciones
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 import asyncio
 from typing import Optional
 
@@ -28,11 +28,11 @@ async def Hola():
     return {"mensaje": "¡Hola Mundo FastAPI!",
             "estatus":"200"
             }      
-@app.get("/v1/usuario/{id}",tags=["Parametro Obligatorio"])
+@app.get("/v1/parametroOb/{id}",tags=["Parametro Obligatorio"])
 async def consultaUno(id:int):
     return{"se encontro usuario": id}
 
-@app.get("/v1/usuarios/",tags=["Parametro Opcional"])
+@app.get("/v1/parametroOp/",tags=["Parametro Opcional"])
 async def consultaTodos(id:Optional[int]=None):
     if id is not None:
         for usuario in usuarios:
@@ -42,5 +42,55 @@ async def consultaTodos(id:Optional[int]=None):
     else:
         return{"mensaje":"No se proporciono id"}   
 
+@app.get("/v1/usuarios/",tags=["CRUD HTTP"])
+async def leer_usuarios():
+    return{
+        "status":"200",
+        "total":len(usuarios),
+        "usuarios":usuarios
+    }
 
+
+
+@app.post("/v1/usuarios/",tags=["CRUD HTTP"],status_code=status.HTTP_201_CREATED)
+async def crear_usuario(usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == usuario.get("id"):
+            raise HTTPException(
+                status_code=400,
+                detail="El id ya existe"
+            )
+    usuarios.append(usuario)
+    return  {
+        "mensaje":"Usuario Agregado",
+        "Usuario":usuario
+        
+    }
+
+@app.put("/v1/usuarios/",tags=["CRUD HTTP"],status_code=status.HTTP_204_NO_CONTENT)
+async def Actualizar_usuario(usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == usuario.get("id"):
+            usuarios.append(usuario)
+            return{
+                "mensaje":"usuario Actualizado",
+                "Usuario":usuario
+            }
+        raise HTTPException(
+            status_code=400,
+            detail="El id no existe"
+        )   
     
+@app.delete("/v1/usuarios/",tags=["CRUD HTTP"])
+async def Eliminar_usuario(usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == usuario.get("id"):
+            usuarios.pop(usuario)
+            return{
+                "mensaje":"usuario Eliminado",
+                "Usuario Elminado":usuario
+            }
+        raise HTTPException(
+            status_code=400,
+            detail="Id no existente"
+        )
